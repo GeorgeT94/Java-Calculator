8d05934e0677b99723b273f5c18f01393368225b
George <ga.timbrell@googlemail.com>
2018-07-11 11:16:00 +0100

    added subtraction test

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -1,15 +1,28 @@
 package com.qa.calculator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+	
+	public Calculator calc;
+	
+	@Before void init() {
+		calc = new Calculator();
+	}
 
 	@Test
 	public void test() {
-		Calculator calc = new Calculator();
+		
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
+	}
+	
+	@Test
+	public void subtractTest() {
+		int actualValue = calc.subtract(2,2);
+		Assert.assertEquals(0, actualValue);
 	}
 
 }
